d03923b747dde8e52d6d90e4b67add83ae6ef074
Anton Karavikou <akaravikou@solvd.com>
2023-04-20 12:46:34 +0300

    getTestCaseTitleTest for TestRail was finished

--- src/test/java/com/solvd/dice/api/ApiTestRailTest.java
+++ src/test/java/com/solvd/dice/api/ApiTestRailTest.java
@@ -2,6 +2,7 @@ package com.solvd.dice.api;
 
 import com.codepine.api.testrail.TestRail;
 import com.codepine.api.testrail.model.*;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -10,7 +11,7 @@ import java.util.List;
 public class ApiTestRailTest {
 
     @Test()
-    public void getTestCasesTest() throws IOException {
+    public void getTestCaseTitleTest() throws IOException {
         TestRail testRail = TestRail.builder("https://modiusqa.testrail.net",
                 "akaravikou@solvd.com",
                 "OsfdBz0Acymkokfg.yia-egy/c.NGEH1tiEx4s2L/").build();
@@ -18,6 +19,6 @@ public class ApiTestRailTest {
         List<CaseField> customCaseFields = testRail.caseFields().list().execute();
         Case testCase = testRail.cases().get(17, customCaseFields).execute();
         String testCaseTitle = testCase.getTitle();
-        Integer testCaseId = testCase.getId();
+        Assert.assertNotNull(testCaseTitle, "Title is absent");
     }
 }
